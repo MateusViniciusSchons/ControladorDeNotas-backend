bfbc455c3563cfdaa31e67180eaf48c78c21ff1a
Mateus <mateusvschons@gmail.com>
2020-05-29 10:35:02 -0300

    adding Procfile

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ const cors = require('cors');
 const routes = require('./routes');
 const app = express();
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
 app.use(routes);
 
