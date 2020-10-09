1797b81b8b850782f6aa937b83c947d073be14f1
Mateus <mateusvschons@gmail.com>
2020-10-09 16:57:33 -0300

    refatorando o código

--- src/controllers/calculationsController.js
+++ src/controllers/calculationsController.js
@@ -2,51 +2,39 @@ module.exports = {
     calculateGrade(req, res) {
         const { notas, media } = req.body;
 
-        let nullGrades = [];
-        let grades = [];
-        let type = "a";
-        let resultado;
+        let nullGrades = []; // Array que armazena todas as notas que não possuem valor
+        let normalGrades = []; // Array que armazena todas as notas que possuem valor
+        let type = "a"; // Armazena o tipo de média a ser calculada
+        let result; // Armazena o resultado
+        
 
-        notas.map(nota => {
-            if(nota.nota === '' || nota.isResponse === true) {
-                nullGrades.push(nota)
-            } else {
-                nota.nota = Number(nota.nota.toString().split(',').join('.'))
-                grades.push(nota)
-            }
+        nullGrades = notas.filter(nota => nota.nota === '' || nota.nota === null || nota.isResponse === true);
+        normalGrades = notas.filter(nota => !nullGrades.includes(nota));
+        // Se todas as notas tiver peso, a média é ponderada
+        type = notas.every(nota => nota.peso !== null && nota.peso !== '')
+            ? 'p'
+            : 'a';
 
-            if(nota.peso !== '') {
-                type = "p";
-            }
-        })
+        if(type === "a") { 
 
-        if(type === "a") { // aritmética
-            let somaGrades = 0;
-            grades.map(grade => {
-                somaGrades += grade.nota;
-            });
-            resultado = ((media * notas.length) - (somaGrades)) / nullGrades.length;
-        } else {  //ponderada
-            let somaGrades = 0; // soma das notas q tem multiplicadas pelo seu peso
-            let somaPesos = 0; // soma dos pesos que não tem sua nota
-            grades.map(grade => {
-                somaGrades += (grade.peso * grade.nota);
-            });
-            nullGrades.map(grade => {
-                somaPesos += grade.peso;
-            })
+            let normalGradesSum = normalGrades.reduce((accumulator, grade) => grade.nota + accumulator, 0);
+
+            result = ((media * notas.length) - (normalGradesSum)) / nullGrades.length;
+        } else {
+
+            let normalGradesSum = normalGrades.reduce((total, grade) => total + (grade.nota * grade.peso), 0);// soma das notas que tem valor multiplicadas pelo seu peso
+            let somaPesos = nullGrades.reduce((total, grade) => total + grade.peso, 0);
             // (valor mínimo da média escolar x 10 (no caso, 70 é a do IFFar) - (soma das notas multiplicadas com os pesos)) / soma dos pesos sem nota 
-            resultado = ((media * 10) - somaGrades) / somaPesos;
+            result = ((media * 10) - normalGradesSum) / somaPesos;
         }
 
-        // nota -> nota que deve tirar;
-        // notas que faltam -> Array de notas que estão nulas;
-        nullGrades.map(grade => {
-            grade.nota = Number(resultado > 0? resultado.toFixed(2): 0);
+        let calculatedGrades = nullGrades;
+        calculatedGrades.map(grade => {
+            grade.nota = Number(result > 0? result.toFixed(2): 0);
             grade.isResponse = true;
         });
 
-        res.json({ notas: nullGrades })
+        res.json({ notas: calculatedGrades })
     },
     calculateFinalAverage(req, res) {
         const { semestre1, media } = req.body;
